9442274478cf46647754171b1335d3d63a01f58f
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-02-11 19:43:00 +0100

    Create Expo App, setup tailwind, and install RN navigation

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./screens/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00CCBB",
+      },
+      // backgroundImage: {
+      //   homebg: "url('./assets/homebg.png')",
+      // },
+      // fontFamily: {
+      //   Poppins: "Poppins",
+      // },
+    },
+  },
+  plugins: [],
+};
